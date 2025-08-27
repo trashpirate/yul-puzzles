59d9db1e9d0489d52a077dc8e82271790650b7c8
trashpirate <nadinaoates@gmail.com>
2025-08-26 20:05:19 -0500

    4 - RevertWithError: solved

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -7,6 +7,11 @@ contract RevertWithError {
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
+            mstore(0x00, shl(224, 0x08c379a0)) // store selector for "Error(string)"
+            mstore(0x04, 0x20) // store offset to string: 32 (0x20) bytes
+            mstore(0x24, 0xc) // length of string: 12 characters => 12 bytes
+            mstore(0x44, "RevertRevert") // store data
+            revert(0x00, 0x80) // return 4 words of 32 bytes
         }
     }
 }
