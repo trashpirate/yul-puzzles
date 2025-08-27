79ce8433878f3324cb3d767038561506e7af0403
trashpirate <nadinaoates@gmail.com>
2025-08-27 10:12:36 -0500

    7 - Double: solved

--- src/Double.sol
+++ src/Double.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract Double {
-
-  function main(uint256 x) external pure returns (uint256) {
-      assembly {
-          // your code here
-          // return 2 * x using assembly
-          // assume x will always be less
-          // than half type(uint256).max
-          // so it won't overflow
-          // hint: x can be directly accessed in assembly
-
-          // see here for how to multiply in YUL: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
-      }
-  }
+    function main(uint256 x) external pure returns (uint256) {
+        assembly {
+            // your code here
+            // return 2 * x using assembly
+            // assume x will always be less
+            // than half type(uint256).max
+            // so it won't overflow
+            // hint: x can be directly accessed in assembly
+            mstore(0x00, mul(x, 2))
+            return(0x00, 0x20)
+            // see here for how to multiply in YUL: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
+        }
+    }
 }
