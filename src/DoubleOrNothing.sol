732a5e5a3103270d2af014bb0ae4499c40f68da8
trashpirate <nadinaoates@gmail.com>
2025-08-27 10:20:57 -0500

    9 - DoubleOrNothing: solved

--- src/DoubleOrNothing.sol
+++ src/DoubleOrNothing.sol
@@ -9,7 +9,10 @@ contract DoubleOrNothing {
             // 21 or less. If 2 * x > 21, then
             // return 0.
             // Hint: check if x ≤ 10, which is equivalent to x < 11
-
+            let y
+            if lt(x, 11) { y := mul(x, 2) }
+            mstore(0x00, y)
+            return(0x00, 0x20)
         }
     }
 }
