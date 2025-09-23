036f75f47488a5d4a9e68c0de4b329212c764631
trashpirate <nadinaoates@gmail.com>
2025-09-22 20:36:17 -0500

    32 - WriteDynamicArrayToStorage: solved

--- src/WriteDynamicArrayToStorage.sol
+++ src/WriteDynamicArrayToStorage.sol
@@ -8,6 +8,24 @@ contract WriteDynamicArrayToStorage {
         assembly {
             // your code here
             // write the dynamic calldata array `x` to storage variable `writeHere`
+
+            // get array length
+            let offset := add(0x04, calldataload(0x04))
+            let length := calldataload(offset)
+
+            // get storage location
+            mstore(0x00, writeHere.slot)
+            let sloc := keccak256(0x00, 0x20)
+
+            // store length
+            sstore(writeHere.slot, length)
+
+            // looping through elements
+            let cdloc := add(0x20, offset)
+            for { let i := 0 } lt(i, length) { i := add(i, 1) } {
+                sstore(add(sloc, i), calldataload(cdloc))
+                cdloc := add(cdloc, 0x20)
+            }
         }
     }
 }
